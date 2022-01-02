a1804723f869d5be187d315dc5725c80f5b21a78
KamilSlobodzian77 <54415177+KamilSlobodzian77@users.noreply.github.com>
2022-01-02 20:52:50 +0100

    Add files via upload
    adding test.ps1

---
+++ test.ps1
@@ -0,0 +1,1 @@
+de-ds-vda0024, de-ds-vda0025, de-ds-vda0026, de-ds-vda0027, de-ds-vda0028, de-ds-vda0029, de-ds-vda0030, de-ds-vda0032, de-ds-vda0034, de-ds-vda0035, de-ds-vda0036, de-ds-vda0037, de-ds-vda0038, de-ds-vda0039, de-ds-vda0040, de-ds-vda0041, de-ds-vda0042, de-ds-vda0043
